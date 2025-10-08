50910e630191d03b4bc8ee0e63d61dbea930afd3
DiceTech453 <amevyematthew@gmail.com>
2025-10-08 11:28:51 +0100

    Compound Interest

--- Compound_interestCalc.py
+++ Compound_interestCalc.py
@@ -5,7 +5,7 @@ rate = 0
 time = 0
 
 while principle <= 0:
-# while true:
+# while True:
     principle = float(input("Enter the principle amount: "))
     if principle <= 0:
         print("Principle can't be less than or equal to zero")
